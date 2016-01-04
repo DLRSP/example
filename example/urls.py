4dd97f5b5576fee4a4ee3eb0933a300f7f1967f5
dlrsp-dev <dlrsp.dev@gmail.com>
2016-01-04 15:48:03 +0100

    copy from main proj

--- example/urls.py
+++ example/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     2. Import the include() function: from django.conf.urls import url, include
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-]
+    url(r'^admin/', admin.site.urls),	
+] + i18n_patterns(
+    url(r'', include('django_errors.urls')),
+)
