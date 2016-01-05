4696f941763e0437511b2602a64ebbe3fa3edf47
dlrsp-dev <dlrsp.dev@gmail.com>
2016-01-05 05:57:43 +0100

    Social Profile Example

--- example/urls.py
+++ example/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.conf.urls.i18n import i18n_patterns
+
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),	
-] + i18n_patterns(
+    url(r'', include('socialprofile.urls')),
+    
+    url(r'^admin/', admin.site.urls),
+	
     url(r'', include('django_errors.urls')),
-)
+]
