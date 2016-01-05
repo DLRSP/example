7fcb9171f643d21fc5f28b7bbe3cf546076e40cc
dlrsp-dev <dlrsp.dev@gmail.com>
2016-01-05 07:46:56 +0100

    package Setup

--- setup.py
+++ setup.py
@@ -1,26 +1,28 @@
 from setuptools import setup
 
-readme = open('README.md').read()
+try:
+    readme = open('README.md').read()
+except:
+    readme = u"Wrapper Views for common errors"
 
-from example import __version__ as version
+from django_errors import __version__ as version
 
 setup(
-    name="example",
+    name="django_errors",
     version=version,
-    url='https://github.com/DLRSP/example',
+    url='https://github.com/DLRSP/django-errors',
     license='MIT',
-    description="Django example Projects",
+    description="Wrapper Views for common errors",
     author='Davide La Rosa',
-    author_email='davide.larosa.coins@gmail.com',
-    packages=['example', ],
+    author_email='dlrsp.py@gmail.com',
+    packages=['django_errors', ],
     long_description=readme,
     include_package_data=True,
     zip_safe=False,
-    dependency_links=['https://github.com/DLRSP/django-errors'],
     install_requires=['django_nose',
-					  'django==1.8.7',
-					  'django-errors',
-					  ],
+                      'django==1.8.7',
+                      'django-errors'
+                      ],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Framework :: Django',
@@ -28,6 +30,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Topic :: Internet :: WWW/HTTP :: BTC ',
+        'Topic :: Internet :: WWW/HTTP',
     ]
 )
+
