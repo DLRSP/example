e70afa2158f737338c9f6ff85e41823617d99f48
dlrsp-dev <dlrsp.dev@gmail.com>
2021-01-27 18:41:58 +0100

    - add requirements - fix pylint reported

--- setup.py
+++ setup.py
@@ -1,11 +1,29 @@
 from setuptools import setup
-
-try:
-    readme = open('README.md').read()
-except:
-    readme = u"Django Example Projects"
-
 from example import __version__ as version
+
+with open('requirements.txt') as fd:
+    requirements = [line.strip() for line in fd if line.strip()]
+
+testing_requirements = [
+    'codecov',
+    'django_nose',
+]
+
+linting_requirements = [
+    'flake8',
+    'pylint',
+    'bandit<1.7',
+]
+
+with open('README.md') as fd:
+    long_description = fd.read()
+
+if 'a' in version:
+    dev_status = '3 - Alpha'
+elif 'b' in version:
+    dev_status = '4 - Beta'
+else:
+    dev_status = '5 - Production/Stable'
 
 setup(
     name="example",
@@ -16,15 +34,18 @@ setup(
     author='DLRSP',
     author_email='dlrsp.dev@gmail.com',
     packages=['example', ],
-    long_description=readme,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     include_package_data=True,
     zip_safe=False,
-    install_requires=['django_nose',
-                      'django<3',
-                      'django-errors',
-                      ],
+    install_requires=requirements,
+    tests_require=testing_requirements,
+    extras_require={
+        'testing': testing_requirements,
+        'linting': linting_requirements,
+    },
     classifiers=[
-        'Development Status :: 4 - Beta',
+        f'Development Status :: {dev_status}',
         'Framework :: Django',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
