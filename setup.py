02b274781b58f44e5b859e9eea15908955a42d4f
dlrsp-dev <dlrsp.dev@gmail.com>
2021-11-22 22:21:54 +0100

    - fix setup requirements

--- setup.py
+++ setup.py
@@ -5,10 +5,11 @@ try:  # for pip >= 10
 except ImportError:  # for pip <= 9.0.3
     from pip.req import parse_requirements
 
-install_reqs = parse_requirements('requirements/py38-django32.txt', session='hack')
+install_reqs = parse_requirements("requirements/py38-django32.txt", session="hack")
 
-reqs = [str(ir.req) for ir in install_reqs]
+try:
+    requirements = [str(ir.req) for ir in install_reqs]
+except Exception:
+    requirements = [str(ir.requirement) for ir in install_reqs]
 
-setup(
-    install_requires=reqs
-)
+setup(install_requires=requirements)
